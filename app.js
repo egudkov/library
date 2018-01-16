ea09d1f055daa04d61229733250c88590b325d98
Evgeny Gudkov <gudkoveyu@gmail.com>
2018-01-16 09:47:38 +0300

    App crashed at heroku. Debug added

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ app.get('/about', function(req, res) {
 });
 
 app.listen(process.env.PORT || 3000, function () {
-    console.log('Listening on port 3000...');
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
 module.exports = app;
